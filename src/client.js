01ce5e670c086db2e79a63fbdda6510c76696fb2
FMakareev <fmakareev@gmail.com>
2017-08-11 19:25:01 +0300

    init

---
+++ src/client.js
@@ -0,0 +1,20 @@
+import React      from 'react';
+import ReactDOM   from 'react-dom';
+import {browserHistory, Router} from 'react-router';
+import routes from './routes';
+import {Provider} from 'react-redux';
+import configureStore from './redux/configureStore';
+import DevTools from './components/DevTools';
+const store = configureStore();
+
+const component = (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            {routes}
+        </Router>
+    </Provider>
+);
+
+ReactDOM.render(component, document.getElementById('react-view'));
+
+ReactDOM.render(<DevTools store={store} />, document.getElementById('dev-tools'));
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import counterReducer from './counterReducer';
+import timeReducer from './timeReducer';
+export default combineReducers({
+    counter: counterReducer,
+    time: timeReducer
+});
